99b474fa0dd0d4a282a6caa9ac3b69e28ab1e1c1
Peter Akaliro <pin4eva@hotmail>
2019-09-23 11:38:34 +0100

    2

---
+++ server/sequelize.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const UserModel = require("./Models/User");
+const PostModel = require("./Models/Post");
+const TagModel = require("./Models/Tag");
+
+const sequelize = require("../db/database");
+
+const User = UserModel(sequelize, Sequelize);
+
+const PostTag = sequelize.define("post_tag", {});
+const Post = PostModel(sequelize, Sequelize);
+const Tag = TagModel(sequelize, Sequelize);
+
+Post.belongsToMany(Tag, { through: PostTag, unique: false });
+Tag.belongsToMany(Post, { through: PostTag, unique: false });
+User.belongsTo(User);
+
+// sequelize.sync({ force: true })
+// .then(()=>)
